40f255f588f0bf1998b66157a806923363025553
annavottak <a.yakovleva@offergate.pro>
2024-03-19 15:51:30 +0300

    fix of comments

--- app/src/androidTest/java/com/example/first/Test1000Click.kt
+++ app/src/androidTest/java/com/example/first/Test1000Click.kt
@@ -2,37 +2,13 @@ package com.example.first
 
 
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.SemanticsActions
-import androidx.compose.ui.semantics.SemanticsProperties
-import androidx.compose.ui.semantics.getOrNull
-import androidx.compose.ui.test.SemanticsMatcher
-import androidx.compose.ui.test.SemanticsNodeInteraction
-import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
 import androidx.compose.ui.test.assertIsNotEnabled
-import androidx.compose.ui.text.TextLayoutResult
 
-fun SemanticsNodeInteraction.assertTextColor(
-    color: Color
-): SemanticsNodeInteraction = assert(isOfColor(color))
-
-private fun isOfColor(color: Color): SemanticsMatcher = SemanticsMatcher(
-    "${SemanticsProperties.Text.name} is of color '$color'"
-) {
-    val textLayoutResults = mutableListOf<TextLayoutResult>()
-    it.config.getOrNull(SemanticsActions.GetTextLayoutResult)
-        ?.action
-        ?.invoke(textLayoutResults)
-    return@SemanticsMatcher if (textLayoutResults.isEmpty()) {
-        false
-    } else {
-        textLayoutResults.first().layoutInput.style.color == color
-    }
-}
 class Test1000Click {
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -42,11 +18,9 @@ class Test1000Click {
         composeTestRule.apply {
             val counter = Counter()
             setContent { CounterScreen(counter) }
-            var minCounter = 0
-            do {
+            for (n in 1..1000) {
                 onNodeWithTag("remove").performClick()
-                minCounter++
-            } while (minCounter < 1000)
+            }
             onNodeWithTag("remove").assertIsNotEnabled()
             onNodeWithTag("counter").assertTextColor(Color.Blue)
         }
@@ -57,11 +31,10 @@ class Test1000Click {
         composeTestRule.apply {
             val counter = Counter()
             setContent { CounterScreen(counter) }
-            var minCounter = 0
-            do {
+            for (n in 1..1000) {
                 onNodeWithTag("add").performClick()
-                minCounter++
-            } while (minCounter < 1000)
+            }
+
             onNodeWithTag("add").assertIsNotEnabled()
             onNodeWithTag("counter").assertTextColor(Color.Red)
         }
